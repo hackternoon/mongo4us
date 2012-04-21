10c050a5e8b3477a69b0df5ee13b1b2e7b33b05b
Dan Bikle <dan@bot4.us>
2012-04-21 19:07:12 +0100

    User_has_many_links_now

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,7 @@
 class Link < ActiveRecord::Base
-  attr_accessible :href, :tag1, :tag2, :tag3, :user_id
+  attr_accessible :href, :tag1, :tag2, :tag3 #, :user_id
+  belongs_to :user
+  def save
+    super if self.user_id.present?
+  end
 end
