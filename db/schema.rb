d494b3ec7f1f6e33a035760a84229f840ae43107
Dan Bikle <dan@bot4.us>
2012-04-21 19:03:32 +0100

    rake_db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120420013128) do
+ActiveRecord::Schema.define(:version => 20120421175251) do
+
+  create_table "links", :force => true do |t|
+    t.string   "href"
+    t.string   "tag1"
+    t.string   "tag2"
+    t.string   "tag3"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
