4f63a355aef86f65887355f2226b37e5c7308bca
Dan Bikle <dan@bot4.us>
2012-04-22 17:01:55 -0700

    links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -41,16 +41,10 @@ class LinksController < ApplicationController
   # POST /links.json
   def create
     @link = Link.new(params[:link])
-
-    respond_to do |format|
-      if @link.save
-        format.html { redirect_to @link, notice: 'Link was successfully created.' }
-        format.json { render json: @link, status: :created, location: @link }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
-      end
-    end
+    debugger
+    @link.user_id = current_user.id
+    @link.save
+    redirect_to links_path
   end
 
   # PUT /links/1
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -5,6 +5,7 @@ describe HomeController do
   describe "GET 'index'" do
     it "returns http success" do
       get 'index'
+debugger
       response.should be_success
     end
   end
